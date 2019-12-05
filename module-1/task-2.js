bf2746f54966c15ac37cdf079c80dfd131c8608d
KirillKulakow <53854091+KirillKulakow@users.noreply.github.com>
2019-12-05 09:34:10 +0200

    Correct task-2

--- module-1/task-2.js
+++ module-1/task-2.js
@@ -8,6 +8,6 @@ const ordered = 80;
 
 if (ordered > total) {
     console.log('На складе недостаточно товаров!');
-} else if (ordered <= total) {
+} else {
     console.log('Заказ оформлен, с вами свяжется менеджер');
 }
